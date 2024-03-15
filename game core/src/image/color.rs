7085a5a8145b4dfffdd277856df30eb4da2ecb4f
Lonekl <yonek@disroot.org>
2024-03-15 17:47:06 +0100

    Added transparent constant, to `Rgba8`.

--- game core/src/image/color.rs
+++ game core/src/image/color.rs
@@ -80,6 +80,7 @@ impl Rgba8 {
 
     pub const WHITE: Self = Self::new(255, 255, 255, 255);
     pub const BLACK: Self = Self::new(0, 0, 0, 255);
+    pub const TRANSPARENT: Self = Self::new(0, 0, 0, 0);
 
     pub const fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
 
